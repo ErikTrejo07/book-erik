f4a8ff86cb3d5d8f46e2020179ae2e9f995eecac
Erik Trejo <erik.trejob@gmail.com>
2024-06-07 16:04:42 -0600

    Descarga de cv completado

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -10,6 +10,7 @@ export class NavbarComponent {
   currentSection: string = 'seccion1';
   isCollapsed = true;
 
+
   @HostListener('window:scroll', [])
   onScroll(): void {
     const sections = document.querySelectorAll('section');
@@ -33,6 +34,18 @@ export class NavbarComponent {
 
   toggleCollapse(): void {
     this.isCollapsed = !this.isCollapsed;
+  }
+
+  descargarArchivo() {
+    const nombreArchivo = 'CV_Erik_Trejo_2024.pdf';  // Nombre del archivo en la carpeta assets
+    const archivoURL = `assets/${nombreArchivo}`;  // Ruta relativa al archivo en la carpeta assets
+
+    const link = document.createElement('a');
+    link.href = archivoURL;
+    link.download = nombreArchivo;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 
 }
